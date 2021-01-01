6bebc67a8db4be94111e49a1c19bd66b5f75367d
Ryoya Takayama <takayamar827@gmail.com>
2021-01-02 00:07:53 +0900

    add: axiosで取得

--- App.tsx
+++ App.tsx
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { ShowNews } from "./modules/ShowNews";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [news, setNews] = useState([]);
+
+    const url = "http://newsapi.org/v2/top-headlines?" + "country=us&" + "apiKey=e29c6ea3a4614492b6c06cb8ac536c34";
+
+    useEffect(() => {
+        const getNews = async () => {
+            try {
+                console.log("成功");
+                const response = await axios.get(url);
+                // console.log(response.data.articles);
+                setNews(response.data.articles);
+            } catch (error) {
+                console.log("失敗");
+                console.log(error);
+            }
+        };
+        console.log(news);
+        getNews();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start workinaag on your app!</Text>
+            <ShowNews />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
---
+++ modules/ShowNews.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export const ShowNews: React.FC = () => {
+    return (
+        <View>
+            <Text>shownewsから</Text>
+        </View>
+    );
+};
